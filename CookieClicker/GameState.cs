9d37078d98b306088db66ee35fbdf0a85507e6a3
agata.heiderova <agata.heiderova@PC-U02-03>
2026-05-19 10:00:13 +0200

    Přidejte soubory projektu.

---
+++ CookieClicker/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClicker
+{
+        public class GameState
+        {
+            public double Cookies { get; set; }
+            public double CookiesPerClick { get; set; }
+            public double CookiesPerSecond { get; set; }
+            public int TotalClicks { get; set; }
+            public int TotalUpgradesBought { get; set; }
+        }
+    }
+
